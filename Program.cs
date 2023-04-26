682640a695288a2be08a7f35bb927ab92baab14e
Peangpen068 <123537577+Peangpen068@users.noreply.github.com>
2023-04-26 15:01:07 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿class program {
+    static void Main(string[] args){
+
+        int TotalStalls = int.Parse(Console.ReadLine());
+        int[] num = new int[TotalStalls];
+        Console.WriteLine("Total Stalls = {0} ", TotalStalls);
+        
+        for (int i = 0; i < TotalStalls; i++){
+            Console.Write("{0} ", num[i]);
+        }
+        while(true){
+
+            Console.WriteLine("Enter Stall's number = ");
+            int stall1 = int.Parse(Console.ReadLine());
+            int stall2 = int.Parse(Console.ReadLine());
+
+            if(stall1 == 0 || stall1 < 0 && stall2 == 0 || stall2 < 0 ){
+                Console.WriteLine("Invalid Input");
+                break;
+            }if(stall1 == 0 || stall2 == 0 || stall2 < 0 || stall1 < 0 ) {
+                total++;
+            } if (stall1 > 0 && stall2 > 0 ) {
+                total++;
+            } else {
+                total++;
+            }
+        }
+        
+    }
+        
+}
